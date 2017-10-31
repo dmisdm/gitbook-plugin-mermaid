8e99d97e1787ac8abf9e972bd134df970b913a05
Daniel Manning <danielmanning213@gmail.com>
2017-11-01 09:48:23 +1100

    Improved styling

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
 const mainTemplate = require("./convert/converter.html");
-const mermaidJs = require("!raw-loader!mermaid/dist/mermaid.min.js");
+const mermaidJs = require("!raw-loader!./convert/mermaid.min.js");
 const initScript = require("!raw-loader!./initScript");
-
-const renderToSvg = mermaidCode => {
+const styleCss = require("!raw-loader!./convert/style.css");
+const themeCss = require("!raw-loader!./convert/theme.css");
+const renderToSvg = ({ mermaidCode, css }) => {
   /* eslint-disable */
   var container = document.getElementById("container");
   var holder = document.getElementById("mermaid-holder");
@@ -11,24 +12,31 @@ const renderToSvg = mermaidCode => {
     holder.removeChild(holder.firstChild);
   }
   var el;
+  var styleEl = document.createElement("style");
+  styleEl.innerHTML = css;
   el = document.createElement("div");
   el.innerHTML = mermaidCode;
   el.className = "mermaid";
+  holder.appendChild(styleEl);
   holder.appendChild(el);
   mermaid.init();
-  var xmlSerializer = new XMLSerializer();
-  svgValue = xmlSerializer.serializeToString(container);
 
-  return svgValue;
+  return holder.outerHTML;
 };
 
-const convertMermaidCodeToSvg = async mermaidCode => {
+const convertMermaidCodeToSvg = async (mermaidCode, book) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(mainTemplate);
   await page.evaluate(mermaidJs);
   await page.evaluate(initScript);
-  const output = await page.evaluate(renderToSvg, mermaidCode);
+
+  const output = await page.evaluate(renderToSvg, {
+    mermaidCode,
+    css: styleCss + themeCss,
+    config: book.config.get("pluginsConfig.mermaid")
+  });
+
   browser.close();
   return output;
 };
@@ -36,7 +44,9 @@ const convertMermaidCodeToSvg = async mermaidCode => {
 module.exports = {
   blocks: {
     mermaid: {
-      process: async block => convertMermaidCodeToSvg(block.body)
+      process: function(block) {
+        return convertMermaidCodeToSvg(block.body, this);
+      }
     }
   }
 };
